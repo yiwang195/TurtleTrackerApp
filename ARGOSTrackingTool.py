6ada6b99f4de4773fddc4a13a0ce572f65a3690f
yiwang195 <yi.wang195@duke.edu>
2020-10-02 18:55:43 -0400

    Parases a pasted data line into variables

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -8,3 +8,19 @@
 # Author: Yi Wang (yi.wang195@duke.edu)
 # Date:   Fall 2020
 #--------------------------------------------------------------
+
+# Copy and paste a line of data as the lineString variable value
+lineString = '20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0'
+
+# Use the split command to parse the items in lineString into a list object
+lineData = lineString.split()
+
+# Extract/Assign variables to specfic items in the list
+record_id = lineData[0]             # ARGOS tracking record ID
+obs_date = lineData[2]   # Observation date
+ob_lc = lineData[4]                 # Observation Location Class
+obs_lat = lineData[6]               # Observation Latitude
+obs_lon = lineData[7]               # Observation Longitude
+
+# Print information to the use
+print (f"Record {record_id} indicates Sara was seen at lat:{obs_lat} and lon:{obs_lon} on {obs_date}")
