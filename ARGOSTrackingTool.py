53938e8c3c739e3519403c1223530c35a4eab318
yiwang195 <yi.wang195@duke.edu>
2020-10-02 19:15:02 -0400

    Reads data in from file

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,18 +9,30 @@
 # Date:   Fall 2020
 #--------------------------------------------------------------
 
-# Copy and paste a line of data as the lineString variable value
-lineString = '20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0'
+#Create a variable pointing to the data file
+file_name = './data/raw/sara.txt'
 
-# Use the split command to parse the items in lineString into a list object
+#Create a file object from the file
+file_object = open(file_name,'r')
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Pretend we read one line of data from the file
+lineString = line_list[100]
+
+#Split the string into a list of data items
 lineData = lineString.split()
 
-# Extract/Assign variables to specfic items in the list
-record_id = lineData[0]             # ARGOS tracking record ID
-obs_date = lineData[2]   # Observation date
-ob_lc = lineData[4]                 # Observation Location Class
-obs_lat = lineData[6]               # Observation Latitude
-obs_lon = lineData[7]               # Observation Longitude
+#Extract items in list into variables
+record_id = lineData[0]
+obs_date = lineData[2]
+obs_lc = lineData[4]
+obs_lat = lineData[6]
+obs_lon = lineData[7]
 
-# Print information to the use
-print (f"Record {record_id} indicates Sara was seen at lat:{obs_lat} and lon:{obs_lon} on {obs_date}")
+#Print the location of sara
+print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
